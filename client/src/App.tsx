84ec39e67c9312ad5b8ee654fb83074d0d7fde15
FateYoRHa <fagyan.att@gmail.com>
2026-07-29 06:40:45 +0800

    feat: add UI components and hooks for improved user interface
    - Introduced Skeleton component for loading states.
    - Added Slider component with customizable properties.
    - Implemented Sonner for toast notifications.
    - Created Switch component for toggle functionality.
    - Developed Table components for structured data display.
    - Added Tabs component for tabbed navigation.
    - Implemented Textarea component for multi-line input.
    - Created ToggleGroup and Toggle components for grouped selections.
    - Added Tooltip component for contextual help.
    - Implemented useIsMobile hook for responsive design.
    - Set up initial project structure with TypeScript configuration.
    - Integrated Vite for build tooling and development.
    - Established API constants and data provider for backend communication.

---
+++ client/src/App.tsx
@@ -0,0 +1,56 @@
+import {
+  Refine,
+  GitHubBanner,
+  WelcomePage,
+  Authenticated,
+} from "@refinedev/core";
+import { DevtoolsPanel, DevtoolsProvider } from "@refinedev/devtools";
+import { RefineKbar, RefineKbarProvider } from "@refinedev/kbar";
+
+import { BrowserRouter, Route, Routes, Outlet } from "react-router";
+import routerProvider, {
+  NavigateToResource,
+  CatchAllNavigate,
+  UnsavedChangesNotifier,
+  DocumentTitleHandler,
+} from "@refinedev/react-router";
+import { dataProvider } from "./providers/data";
+import { useNotificationProvider } from "./components/refine-ui/notification/use-notification-provider";
+import { Toaster } from "./components/refine-ui/notification/toaster";
+import { ThemeProvider } from "./components/refine-ui/theme/theme-provider";
+import "./App.css";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <GitHubBanner />
+      <RefineKbarProvider>
+        <ThemeProvider>
+          <DevtoolsProvider>
+            <Refine
+              dataProvider={dataProvider}
+              notificationProvider={useNotificationProvider()}
+              routerProvider={routerProvider}
+              options={{
+                syncWithLocation: true,
+                warnWhenUnsavedChanges: true,
+                projectId: "TjbnRO-SfdpCL-ApDR7D",
+              }}
+            >
+              <Routes>
+                <Route index element={<WelcomePage />} />
+              </Routes>
+              <Toaster />
+              <RefineKbar />
+              <UnsavedChangesNotifier />
+              <DocumentTitleHandler />
+            </Refine>
+            <DevtoolsPanel />
+          </DevtoolsProvider>
+        </ThemeProvider>
+      </RefineKbarProvider>
+    </BrowserRouter>
+  );
+}
+
+export default App;
