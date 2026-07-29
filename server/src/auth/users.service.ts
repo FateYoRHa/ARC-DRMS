d67e2520d986cc340ad310c022dda3249deb2798
FateYoRHa <fagyan.att@gmail.com>
2026-07-29 08:42:55 +0800

    feat(users): add register user controller and service

---
+++ server/src/auth/users.service.ts
@@ -0,0 +1,22 @@
+import { db } from "../database/db";
+import { eq } from "drizzle-orm";
+import { users } from "../database/schema";
+export async function registerUserService(
+  name: string,
+  email: string,
+  password: string,
+) {
+  const emailExists = await db
+    .select()
+    .from(users)
+    .where(eq(users.email, email));
+  if (emailExists.length > 0) {
+    const error = new Error("Email already exists") as Error & {
+      status: number;
+    };
+    error.status = 409;
+    throw error;
+  }
+  const user = await db.insert(users).values({ name, email, password });
+  return user;
+}
