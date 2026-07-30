dbcf949aaca90526efe6e57532fb86c84bb09671
FateYoRHa <fagyan.att@gmail.com>
2026-07-30 09:50:09 +0800

    Refactor database schema for scalability and readability (#4)
    * refactor: update database schema and migration paths; remove obsolete files
    
    * refactor(database): separate schema for scalability and readability
    
    - schema.ts will now export every schema's

--- server/drizzle.config.ts
+++ server/drizzle.config.ts
@@ -7,8 +7,8 @@ if (!process.env.DATABASE_URL) {
 }
 
 export default defineConfig({
-  schema: "./src/database/schema.ts", // Your schema file path
-  out: "./drizzle", // Your migrations folder
+  schema: "./src/database/schema", // Your schema file path
+  out: "./src/database/migrations",
   dialect: "postgresql",
   dbCredentials: {
     url: process.env.DATABASE_URL,
