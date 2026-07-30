f672f81ecb548101d4f40040d6a08f0ff01d57c4
FateYoRHa <fagyan.att@gmail.com>
2026-07-30 14:41:21 +0800

    fix(admission): fix admission to admissions

--- server/src/validators/zod/admission.zod.ts
+++ server/src/validators/zod/admission.zod.ts
@@ -1,6 +1,6 @@
 import { createInsertSchema, createUpdateSchema } from "drizzle-orm/zod";
-import { admission } from "../../database/schema/admissions";
+import { admissions } from "../../database/schema/admissions";
 
 // THESE Generate Zod schemas from the database schema.
-export const admissionInsertSchema = createInsertSchema(admission);
-export const admissionUpdateSchema = createUpdateSchema(admission);
+export const admissionInsertSchema = createInsertSchema(admissions);
+export const admissionUpdateSchema = createUpdateSchema(admissions);
