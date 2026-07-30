676e43c9514d771b28669a7d3d17976c6502b8ea
FateYoRHa <fagyan.att@gmail.com>
2026-07-30 09:45:36 +0800

    refactor: update database schema and migration paths; remove obsolete files

--- server/drizzle/0000_blushing_selene.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE "users" (
-	"id" serial PRIMARY KEY NOT NULL,
-	"name" varchar NOT NULL,
-	"email" varchar NOT NULL,
-	"password" varchar NOT NULL,
-	"role" varchar DEFAULT 'user',
-	"created_at" timestamp DEFAULT now()
-);
