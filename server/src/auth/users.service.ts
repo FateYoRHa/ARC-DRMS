3055046b13848a78e5770f95d86480a770d44fb9
FateYoRHa <fagyan.att@gmail.com>
2026-08-01 09:28:24 +0800

    feat(users): implement error handling for email conflicts in user registration

--- server/src/auth/users.service.ts
+++ server/src/auth/users.service.ts
@@ -1,6 +1,7 @@
 import { db } from "../database/db";
 import { eq } from "drizzle-orm";
 import { users } from "../database/schema";
+import { AppError } from "../errors/AppError";
 export async function registerUserService(
   name: string,
   email: string,
@@ -11,11 +12,7 @@ export async function registerUserService(
     .from(users)
     .where(eq(users.email, email));
   if (emailExists.length > 0) {
-    const error = new Error("Email already exists") as Error & {
-      status: number;
-    };
-    error.status = 409;
-    throw error;
+    throw new AppError(409, "Email already exists");
   }
   return await db.insert(users).values({ name, email, password }).returning();
 }
