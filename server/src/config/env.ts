fa2e7cedb7296b32c7979cdac7713659d394e072
FateYoRHa <fagyan.att@gmail.com>
2026-08-02 09:53:51 +0800

    feat(auth): add environment variables for JWT token management

--- server/src/config/env.ts
+++ server/src/config/env.ts
@@ -5,6 +5,10 @@ dotenv.config();
 const env = cleanEnv(process.env, {
   DATABASE_URL: str(),
   PORT: port({ default: 5000 }),
+  ACCESS_TOKEN_SECRET: str(),
+  REFRESH_TOKEN_SECRET: str(),
+  ACCESS_TOKEN_EXPIRES: str(),
+  REFRESH_TOKEN_EXPIRES: str(),
 });
 
 export default env;
