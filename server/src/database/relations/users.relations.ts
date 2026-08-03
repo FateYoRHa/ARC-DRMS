15b0d40e0d7bc39130aa93f48fff300682b07ae1
FateYoRHa <fagyan.att@gmail.com>
2026-08-04 06:54:44 +0800

    refactor(auth): renamed relations into users.relations and moved to designated folder for relations
    this commit is to ensure scalability and easier file management

--- server/src/database/relations/users.relations.ts
+++ server/src/database/relations/users.relations.ts
@@ -1,7 +1,7 @@
 import { relations } from "drizzle-orm";
 
-import { users } from "./users";
-import { user_sessions } from "./user_sessions";
+import { users } from "../schema/users";
+import { user_sessions } from "../schema/user_sessions";
 
 export const userSessionsRelations = relations(user_sessions, ({ one }) => ({
   user: one(users, {
