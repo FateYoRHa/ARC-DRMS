b718125550e3089f95d787630ae15800f2c24b57
FateYoRHa <fagyan.att@gmail.com>
2026-08-04 06:21:18 +0800

    feat(auth): implement authentication with JWT and enhance user management (#17)
    * feat(auth): add environment variables for JWT token management
    
    * feat(auth): implement roles enum and update users schema
    
    - role column is now set to notNull, this is to avoid type error in service
    
    * feat(auth): enhance environment variable validation for token expiration
    
    * feat(auth): implement access token generation using JWT
    
    * feat(auth): add refresh token generation functionality
    
    * refactor(auth): move sign refresh and access tokens to tokens.ts
    
    - removed jwt.ts for redundancy
    - implemented changes to auth.service
    
    * feat(auth): add verification for access and refresh tokens
    
    * feat(auth): add custom user type to Express request interface, define AuthenticatedUser, AccessTokenPayload, and RefreshTokenPayload interfaces
    
    * fix(tsconfig): update include paths to specify TypeScript files
    
    * feat(auth): export User type inferred from users table schema
    
    * feat(auth): update verifyAccessToken to return AccessTokenPayload type
    
    * feat(auth): add authenticate accesstoken middleware
    
    * feat(auth): create user_sessions schema for session management
    
    * feat(auth): define users and user_sessions relationship
    
    * fix(auth): resolve ReferenceError when running npx drizzle-kit generate
    
    - created relations.ts to mitigate circular dependency
    
    * feat(auth): add user_sessions table and update migration journal

--- server/src/config/env.ts
+++ server/src/config/env.ts
@@ -1,10 +1,18 @@
-import { cleanEnv, str, port, url } from "envalid";
+import { cleanEnv, str, port } from "envalid";
 import dotenv from "dotenv";
 dotenv.config();
 
 const env = cleanEnv(process.env, {
   DATABASE_URL: str(),
   PORT: port({ default: 5000 }),
+  ACCESS_TOKEN_SECRET: str(),
+  REFRESH_TOKEN_SECRET: str(),
+  ACCESS_TOKEN_EXPIRES: str({
+    choices: ["15m", "1h", "7d"],
+  }),
+  REFRESH_TOKEN_EXPIRES: str({
+    choices: ["7d", "14d", "30d"],
+  }),
 });
 
 export default env;
--- server/src/database/schema/users.ts
+++ server/src/database/schema/users.ts
@@ -4,14 +4,19 @@ import {
   varchar,
   boolean,
   timestamp,
+  pgEnum,
 } from "drizzle-orm/pg-core";
+
+export const roles = pgEnum("roles", ["user", "admin", "guest", "student"]);
 
 export const users = pgTable("users", {
   id: serial("id").primaryKey(),
   name: varchar("name").notNull(),
   email: varchar("email").notNull(),
   password: varchar("password").notNull(),
-  role: varchar("role").default("user"),
+  role: roles("role").default("user").notNull(),
   is_active: boolean("is_active").default(true),
   createdAt: timestamp("created_at").defaultNow(),
 });
+export type User = typeof users.$inferSelect;
+
