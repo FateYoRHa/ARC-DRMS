5ebcca64c2e57262fae34dfc603f209112e5fc7f
FateYoRHa <fagyan.att@gmail.com>
2026-08-03 08:56:37 +0800

    refactor(auth): move sign refresh and access tokens to tokens.ts
    - removed jwt.ts for redundancy
    - implemented changes to auth.service

--- server/src/utils/token.ts
+++ server/src/utils/token.ts
@@ -1,26 +1,30 @@
-import { signAccessToken, signRefreshToken } from "../config/jwt";
+import jwt from "jsonwebtoken";
+import type { StringValue } from "ms";
 import env from "../config/env";
-export function generateAccessToken(user: {
-  id: number;
-  role: string;
-  session_id: string;
-  token_version: number;
-}) {
-  return signAccessToken(
-    user,
-    env.ACCESS_TOKEN_SECRET,
-    env.ACCESS_TOKEN_EXPIRES,
-  );
+
+export function signAccessToken(
+  payload: {
+    id: number;
+    role: string;
+    session_id: string;
+    token_version: number;
+  }
+) {
+  const accessToken = jwt.sign(payload, env.ACCESS_TOKEN_SECRET, {
+    expiresIn: env.ACCESS_TOKEN_EXPIRES as StringValue,
+  });
+  return accessToken;
 }
 
-export function generateRefreshToken(user: {
-  id: number;
-  session_id: string;
-  token_version: number;
-}) {
-  return signRefreshToken(
-    user,
-    env.REFRESH_TOKEN_SECRET,
-    env.REFRESH_TOKEN_EXPIRES,
-  );
+export function signRefreshToken(
+  payload: {
+    id: number;
+    session_id: string;
+    token_version: number;
+  }
+) {
+  const refreshToken = jwt.sign(payload, env.REFRESH_TOKEN_SECRET, {
+    expiresIn: env.REFRESH_TOKEN_EXPIRES as StringValue,
+  });
+  return refreshToken;
 }
