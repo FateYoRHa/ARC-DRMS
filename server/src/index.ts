7f16d6490960f4920ac383b0d1032ce9ce53a1ac
FateYoRHa <fagyan.att@gmail.com>
2026-08-04 13:38:37 +0800

    feat(cors): configure CORS middleware with specific origin and credentials

--- server/src/index.ts
+++ server/src/index.ts
@@ -15,7 +15,12 @@ const port = Number(env.PORT ?? 5000);
 
 // middlewares
 app.use(express.json());
-app.use(cors())
+app.use(
+  cors({
+    origin: ["http://localhost:5173"],
+    credentials: true,
+  }),
+);
 app.use(helmet());
 app.use(cookieParser());
 
@@ -36,7 +41,6 @@ app.use(notFoundMiddleware); // Handles requests that don't match any registered
 
 // GLOBAL ERROR HANDLER, KEEP THIS LAST
 app.use(errorMiddleware);
-
 
 // --------------------
 // SERVER START
