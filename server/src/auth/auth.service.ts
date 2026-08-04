063f37b7207af7e74b3d5435a2f7edd6b9eaf5b1
FateYoRHa <fagyan.att@gmail.com>
2026-08-04 13:25:04 +0800

    feat(auth): add refresh access token service functionality
    - added /refresh api endpoint

--- server/src/auth/auth.service.ts
+++ server/src/auth/auth.service.ts
@@ -1,5 +1,5 @@
 import { db } from "../database/db";
-import { eq } from "drizzle-orm";
+import { eq, and } from "drizzle-orm";
 
 import { users, user_sessions } from "../database/schema";
 import { AppError } from "../errors/AppError";
@@ -8,6 +8,7 @@ import { comparePassword } from "../utils/password";
 import { RegisterUserInput } from "./validators/auth.api";
 
 import {
+  compareRefreshToken,
   hashRefreshToken,
   signAccessToken,
   signRefreshToken,
@@ -143,4 +144,78 @@ export async function logoutService(refreshToken: string) {
   return await db
     .delete(user_sessions)
     .where(eq(user_sessions.session_id, token.session_id));
+}
+
+export async function refreshTokenService(
+  refreshToken: string,
+  ip_address: string,
+  user: string,
+) {
+  if (!refreshToken) {
+    throw new AppError(401, "Token not recognized");
+  }
+  const payload = verifyRefreshToken(refreshToken);
+
+  const hashedToken = await db
+    .select({ refresh_token: user_sessions.refresh_token })
+    .from(user_sessions)
+    .where(
+      and(
+        eq(user_sessions.session_id, payload.session_id),
+        eq(user_sessions.user_id, payload.id),
+      ),
+    )
+    .limit(1)
+    .then((rows) => rows[0]);
+
+  if (!hashedToken) {
+    throw new AppError(401, "Token not recognized.");
+  }
+
+  const decodedToken = await compareRefreshToken(
+    refreshToken,
+    hashedToken.refresh_token,
+  );
+  if (!decodedToken) {
+    throw new AppError(401, "Token not recognized.");
+  }
+  await db
+    .delete(user_sessions)
+    .where(
+      and(
+        eq(user_sessions.session_id, payload.session_id),
+        eq(user_sessions.user_id, payload.id),
+      ),
+    );
+  const sessionId = crypto.randomUUID();
+  const newRefreshToken = signRefreshToken({
+    id: payload.id,
+    session_id: sessionId,
+    token_version: 1,
+  });
+  const refreshTokenValues = verifyRefreshToken(newRefreshToken);
+  const hashedRefreshToken = await hashRefreshToken(newRefreshToken);
+  await db
+    .insert(user_sessions)
+    .values({
+      session_id: refreshTokenValues.session_id,
+      user_id: payload.id,
+      refresh_token: hashedRefreshToken,
+      token_version: refreshTokenValues.token_version,
+      user_agent: "Temporary user agent", //! to be changed later, comes from header
+      ip_address: ip_address,
+      expires_at: new Date(refreshTokenValues.exp * 1000), //* this makes it so that instead of number .exp date time will be saved to database
+      last_used_at: new Date(refreshTokenValues.iat * 1000),
+      revoked_at: null,
+    })
+    .returning();
+
+  const newAccessToken = signAccessToken({
+    id: payload.id,
+    role: user,
+    session_id: sessionId,
+    token_version: 1,
+  });
+
+  return { accessToken: newAccessToken, refreshToken: newRefreshToken };
 }
