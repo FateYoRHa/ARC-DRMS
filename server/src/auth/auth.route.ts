f60d8fe8a5b115c01c83fc770bd68ed86c00ff9e
FateYoRHa <fagyan.att@gmail.com>
2026-08-04 13:28:03 +0800

    fix(auth): correct import name for auth routes in index file

--- server/src/auth/auth.route.ts
+++ server/src/auth/auth.route.ts
@@ -6,6 +6,6 @@ const router = express.Router();
 
 router.post("/register", validate(registerSchema), userRoutes.registerUser);
 router.post("/login", validate(loginSchema), userRoutes.login);
-
+router.post("/logout", userRoutes.logout);
 router.post("/refresh", userRoutes.refreshAccessToken);
 export default router;
