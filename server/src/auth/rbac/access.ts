54cc22bf8896d0cef3650562c3513a7beb829fc6
FateYoRHa <fagyan.att@gmail.com>
2026-08-06 11:31:26 +0800

    feat(auth): implement role-based access control and update default user role (#23)
    * fix(users): update default role value to use roles constant
    
    * feat(auth): implement role-based access control with role validation and authorization middleware
    
    * feat(auth): add hasAnyRole and requireAnyRole functions for role validation
    
    * feat(auth): add permission validation functions for role-based access control
    
    - add hasPermissions to check if role has permission to access/perform multiple actions

--- server/src/auth/rbac/access.ts
+++ server/src/auth/rbac/access.ts
@@ -8,3 +8,17 @@ export function hasPermission(role: Roles, permission: Permission) {
     false
   );
 }
+
+export function hasPermissions(role: Roles, permissions: Permission[]) {
+  const rolePermissions = role_permissions[role] as readonly Permission[];
+
+  return permissions.every((permission) => rolePermissions.includes(permission));
+}
+
+export function hasRole(role: Roles, required: Roles) {
+  return role === required;
+}
+
+export function hasAnyRole(role: Roles, required: Roles[]) {
+  return required.includes(role);
+}
