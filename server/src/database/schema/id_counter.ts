d7c4565e673915812885cd54b6f55576deae41ec
FateYoRHa <fagyan.att@gmail.com>
2026-08-08 11:02:35 +0800

    feat(admissions): implement status update (#27)
    * feat(database): integrate schema into drizzle initialization
    
    * feat(admissions): update application_id column type to varchar and add migration files
    
    * feat(admissions): remove application_id from create admission field schema
    
    - this is because applicatiion_id will be assigned in for_id service
    
    * refactor(students): change sexes export to a local constant
    
    * feat(database): add id_counters table and schema integration
    
    * feat(admissions): implement admission status update workflow and application ID generation

---
+++ server/src/database/schema/id_counter.ts
@@ -0,0 +1,6 @@
+import { integer, pgTable } from "drizzle-orm/pg-core";
+
+export const admissionIdCounters = pgTable("id_counters", {
+  year: integer("year").primaryKey(),
+  lastNumber: integer("last_number").notNull().default(0),
+});
