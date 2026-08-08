11e46dd574b0159d270a81e9823b836b41f1917f
FateYoRHa <fagyan.att@gmail.com>
2026-08-08 10:03:16 +0800

    feat(database): integrate schema into drizzle initialization

--- server/src/database/db.ts
+++ server/src/database/db.ts
@@ -1,6 +1,7 @@
 import env from "../config/env";
 import { drizzle } from "drizzle-orm/neon-http";
 import { neon } from "@neondatabase/serverless";
+import * as schema from "./schema";
 
 const client = neon(env.DATABASE_URL!);
-export const db = drizzle(client);
+export const db = drizzle(client, { schema });
