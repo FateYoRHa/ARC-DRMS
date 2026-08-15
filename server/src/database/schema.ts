c9bc3e782606ca13fd39ead17ce2191f2e725353
FateYoRHa <fagyan.att@gmail.com>
2026-08-15 13:44:39 +0800

    feat(schema): update schema exports to include curriculum and class-related modules

--- server/src/database/schema.ts
+++ server/src/database/schema.ts
@@ -3,7 +3,16 @@ export * from "./schema/students/students";
 export * from "./schema/students/admissions";
 export * from "./schema/auth/user_sessions";
 export * from "./schema/id_counter";
+
 export * from "./schema/academic/programs";
 export * from "./schema/academic/schools";
 export * from "./schema/academic/curriculum";
 export * from "./schema/academic/school_years";
+export * from "./schema/academic/curriculum_programs";
+export * from "./schema/academic/subjects";
+
+export * from "./schema/classes/sections";
+export * from "./schema/classes/classes";
+export * from "./schema/classes/schedules";
+
+export * from "./schema/teachers/teachers";
