c805f67da7e159a4f03b4ddc258a3f1510a09e65
FateYoRHa <fagyan.att@gmail.com>
2026-08-15 09:44:22 +0800

    fix(admissions): resolve typo in guardian_phone_number field in admissions table (#31)
    * fix(admissions): correct typo in guardian_phone_number and updatedAt fields
    
    * fix(admissions): correct typo in guardian_phone_number field

---
+++ server/src/database/migrations/0018_conscious_bill_hollister.sql
@@ -0,0 +1,2 @@
+ALTER TABLE "admissions" RENAME COLUMN "guadian_phone_number" TO "guardian_phone_number";--> statement-breakpoint
+ALTER TABLE "admissions" RENAME COLUMN "updatedAt" TO "updated_at";
