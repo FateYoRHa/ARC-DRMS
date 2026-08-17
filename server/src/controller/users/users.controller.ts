f871b88f3ff6b42572cdb5d1983f1cb45c25bade
FateYoRHa <fagyan.att@gmail.com>
2026-08-17 13:26:47 +0800

    refactor(users): rename controller functions for consistency

--- server/src/controller/users/users.controller.ts
+++ server/src/controller/users/users.controller.ts
@@ -1,12 +1,12 @@
 import type { Request, Response } from "express";
 import * as usersService from "../../service/index.services";
 
-export async function getAllUsersController(req: Request, res: Response) {
+export async function getAllUsers(req: Request, res: Response) {
   const allUsers = await usersService.getAllUsersService();
   res.status(200).json(allUsers);
 }
 
-export async function getUserByIdController(req: Request, res: Response) {
+export async function getUserById(req: Request, res: Response) {
   const { id } = req.params;
   const user = await usersService.getUserByIdService(Number(id));
   res.status(200).json(user);
