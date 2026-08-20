f832b8a26bdf287f7bafef86f9cc81678acbc8f3
FateYoRHa <fagyan.att@gmail.com>
2026-08-20 08:14:26 +0800

    refactor(teachers): update route handlers to improve naming consistency

--- server/src/routes/teachers.route.ts
+++ server/src/routes/teachers.route.ts
@@ -3,7 +3,7 @@ import * as teachers from "../controller/teachers/teachers.controller";
 
 const router = express.Router();
 
-router.get("/teachers", teachers.getTeachers);
-router.get("/teachers/:id", teachers.getTeacherById);
+router.get("/teachers", teachers.retrieveTeachers);
+router.get("/teachers/:id", teachers.retrieveTeacherById);
 
 export default router;
