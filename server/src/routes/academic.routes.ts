93349ebda5618a2cb56ed4f5312b32071c09ef0f
FateYoRHa <fagyan.att@gmail.com>
2026-08-21 07:42:51 +0800

    feat(programs-module): add comments for program routes in academic.routes.ts

--- server/src/routes/academic.routes.ts
+++ server/src/routes/academic.routes.ts
@@ -2,7 +2,9 @@ import express from "express";
 import * as programRoutes from "../controller/academic/programs.controller";
 
 const router = express.Router();
-
+// Program Routes
+router.get("/programs", programRoutes.retrieveAllPrograms);
+router.get("/programs/:id", programRoutes.retrieveProgramById);
 router.post("/programs", programRoutes.createProgram);
 router.put("/programs/:id", programRoutes.updateProgram);
 export default router;
