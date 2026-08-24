3b36095f0fbd0c26b7c7d18b4946c8963a9e9043
FateYoRHa <fagyan.att@gmail.com>
2026-08-24 18:40:03 +0800

    feat(curriculum_subjects): add routes for retrieving and creating curriculum subjects

--- server/src/routes/academic.routes.ts
+++ server/src/routes/academic.routes.ts
@@ -2,6 +2,7 @@ import express from "express";
 import * as programRoutes from "../controller/academic/programs.controller";
 import * as schoolRoutes from "../controller/academic/schools.controller";
 
+import * as curriculumSubjectsRoutes from "../controller/academic/curriculum_subjects.controller";
 const router = express.Router();
 // Program Routes
 router.get("/programs", programRoutes.retrieveAllPrograms);
@@ -18,5 +19,15 @@ router.post("/schools", schoolRoutes.createSchool);
 router.put("/schools/:id", schoolRoutes.updateSchool);
 router.patch("/schools/:id", schoolRoutes.archiveSchool);
 router.patch("/schools/:id", schoolRoutes.restoreSchool);
+
+// Curriculum Subject Routes
+router.get(
+  "/curriculum-subjects/:id",
+  curriculumSubjectsRoutes.retrieveCurriculumSubjectsController,
+);
+router.post(
+  "/curriculum-subjects",
+  curriculumSubjectsRoutes.addCurriculumSubjectController,
+);
 
 export default router;
